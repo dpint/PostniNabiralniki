1c0edf596b040851084b6929bcff1bf90232c161
Matic Pintarič <matic.pintaric@outlook.com>
2019-03-28 09:44:29 +0100

    Added display on map

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ var mymap = L.map('mapid').setView([46.056946, 14.505751], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
 }).addTo(mymap);
+
+$.getJSON("py/nabiralniki.json", function(data) {
+    $.each(data, function(i, item) {
+        L.marker([data[i].lat, data[i].lon]).addTo(mymap);
+    });   
+});
